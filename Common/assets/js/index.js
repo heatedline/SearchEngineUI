2aaaabf6ce64857b1663a3cea55d05e0b6cb179c
supratim <supratim.c@finlabsindia.com>
2020-04-26 19:25:45 +0530

    updated index

--- Common/assets/js/index.js
+++ Common/assets/js/index.js
@@ -1,12 +1,20 @@
 $(document).ready(function() {
 
-  $("#inpt_search").on('focus', function () {
-  	$(this).parent('label').addClass('active');
+  $("#inpt_search").on('focus', function() {
+    $(this).parent('label').addClass('active');
   });
 
-  $("#inpt_search").on('blur', function () {
-  	if($(this).val().length == 0)
-  		$(this).parent('label').removeClass('active');
+  $("#inpt_search").on('blur', function() {
+    if ($(this).val().length == 0)
+      $(this).parent('label').removeClass('active');
+  });
+
+  $(".search__input").on("keydown", function search(e) {
+    if (e.keyCode == 13) {
+        if($(this).val() != "") {
+          
+        }
+    }
   });
 
 
